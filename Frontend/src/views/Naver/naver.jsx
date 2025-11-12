72ced0b2c32b3e8278fc88a1a89ef2732a698abd
vivek-actowiz-solutions <vivek.panakhaniya@actowiz.co.in>
2025-11-12 15:13:43 +0530

    make design responsive

--- Frontend/src/views/Naver/naver.jsx
+++ Frontend/src/views/Naver/naver.jsx
@@ -50,8 +50,9 @@ const options = [
 const DashDefault = () => {
   const navigate = useNavigate();
   const [data, setData] = useState([]);
-  const [LatencyStats, setLatencyStats] = useState("");
+  const [LatencyStats, setLatencyStats] = useState('');
   const [dataloading, setDataLoading] = useState(false);
+  const [latencydataloading, setlatencyDataLoading] = useState(false);
 
   const [selected, setSelected] = useState([
     {
@@ -117,9 +118,9 @@ const DashDefault = () => {
       setDataLoading(false);
     }
   };
-    const fetchDashboardDataLatencyStats = async () => {
+  const fetchDashboardDataLatencyStats = async () => {
     try {
-      setDataLoading(true);
+      setlatencyDataLoading(true);
 
       // Handle selected IDs
       const ids = Array.isArray(selected) ? selected.map((item) => item.value) : selected ? [selected.value] : [];
@@ -160,7 +161,7 @@ const DashDefault = () => {
     } catch (err) {
       console.error('Error fetching dashboard data:', err);
     } finally {
-      setDataLoading(false);
+      setlatencyDataLoading(false);
     }
   };
   const getIntervalMs = (interval) => {
@@ -172,7 +173,7 @@ const DashDefault = () => {
       case '15m':
         return 900000;
       default:
-        return 30000; 
+        return 30000;
     }
   };
 
@@ -181,7 +182,7 @@ const DashDefault = () => {
       fetchDashboardData();
       fetchDashboardDataLatencyStats(); // Initial fetch
       const interval = setInterval(() => {
-           const [start, end] = selectedDateTime.fn();
+        const [start, end] = selectedDateTime.fn();
         setRange([start, end]);
         fetchDashboardData();
         fetchDashboardDataLatencyStats();
@@ -191,8 +192,8 @@ const DashDefault = () => {
       return () => clearInterval(interval);
     }
     fetchDashboardData();
-    fetchDashboardDataLatencyStats()
-  }, [refreshInterval , range ,selected]);
+    fetchDashboardDataLatencyStats();
+  }, [refreshInterval, range, selected]);
 
   const formatNumber = (num) => {
     if (num >= 1_000_000_000) return (num / 1_000_000_000).toFixed(1).replace(/\.0$/, '') + 'B';
@@ -200,6 +201,17 @@ const DashDefault = () => {
     // if (num >= 1_0000) return (num / 1_000).toFixed(1).replace(/\.0$/, '') + 'K';
     return num;
   };
+
+
+  const countStatusCodes = [
+           { code: 200, color: 'text-success', value: data.status_200 },
+        { code: 502, color: 'text-danger', value: data.status_502 },
+        { code: 500, color: 'text-danger', value: data.status_500 },
+        { code: 400, color: 'text-info', value: data.status_400 },
+        { code: 404, color: 'text-success', value: data.status_404 },
+        { code: 401, color: 'text-info', value: data.status_401 },
+        { code: 403, color: 'text-info', value: data.status_403 },
+  ]
   return (
     <React.Fragment>
       {/* <DashboardTour /> */}
@@ -308,7 +320,6 @@ const DashDefault = () => {
                 value={{ label: refreshInterval, value: refreshInterval }}
                 onChange={(selectedOption) => setRefreshInterval(selectedOption.value)}
                 options={[
-            
                   { value: '1m', label: '1 min' },
                   { value: '5m', label: '5 min' },
                   { value: '15m', label: '15 min' },
@@ -324,9 +335,9 @@ const DashDefault = () => {
           <Button variant="outline-dark" onClick={fetchDashboardData}>
             <FcSearch /> Search
           </Button>
-          <Button variant="outline-dark" onClick={handleClear}>
+          {/* <Button variant="outline-dark" onClick={handleClear}>
             <AiOutlineClear /> Clear
-          </Button>
+          </Button> */}
         </Col>
       </Row>
       <Box sx={{ width: '100%', mb: 3 }}>{dataloading && <LinearProgress />}</Box>
@@ -367,11 +378,11 @@ const DashDefault = () => {
           </Row>
         </Card.Body>
       </Card>
-      <Row className="mt-4">
+      <Row className="mt-4" >
         <Col xl={9} className="mb-4">
-          <Card className="p-3 shadow-sm rounded-3">
+          <Card className="p-3 shadow-sm rounded-3 h-100">
             <Card.Title className="mb-4">Ratios</Card.Title>
-            <Row>
+            <Row className='g-3'>
               {/* 1️⃣ Success Ratio */}
               <Col xl={4} lg={4} md={6} sm={12} className="mb-4">
                 <GaugeComponent
@@ -412,7 +423,6 @@ const DashDefault = () => {
                   }}
                 />
                 <div
-                  
                   style={{
                     fontSize: '25px',
                     fontWeight: '500',
@@ -531,22 +541,14 @@ const DashDefault = () => {
             </Row>
           </Card>
         </Col>
+      
+      
+        <Col xl={3}  className="mb-4" >
 
-        <Col xl={3} className="mb-4">
-          <Card className="p-3 shadow-sm rounded-3">
+        {latencydataloading &&<Box sx={{ width: '100%', mb: 3 }}>{latencydataloading && <LinearProgress />}</Box> }
+        
+          <Card className="p-3 shadow-sm rounded-3 h-100" >
             <Card.Title className="mb-4">Avarage Response Time</Card.Title>
-            <div
-              style={{
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'flex-end',
-                gap: '6px',
-                fontSize: '16px',
-                fontWeight: '500',
-                marginTop: '8px',
-                marginRight: '10px'
-              }}
-            ></div>
             <GaugeComponent
               className="p-0"
               type="grafana"
@@ -574,7 +576,7 @@ const DashDefault = () => {
                 tickLines: { hide: true }
               }}
               // value={items?.successPercentage ? items.successPercentage : 0}
-              value={LatencyStats? parseFloat(LatencyStats.avg_latency_sec) : 0}
+              value={LatencyStats ? parseFloat(LatencyStats.avg_latency_sec) : 0}
               valueLabel={{
                 formatTextValue: (val) => `${val}%`,
                 style: {
@@ -586,67 +588,40 @@ const DashDefault = () => {
             />
             <div
               style={{
-                fontSize: '30px',
+                fontSize: '25px',
                 fontWeight: '500',
                 marginTop: '8px',
                 textAlign: 'center',
                 padding: '5px'
               }}
             >
-              Avarage Response Time
+              Response Time
             </div>
           </Card>
         </Col>
       </Row>
-      <Card className="shadow-sm border-1 rounded-3  p-1">
-        <Card.Title className="mb-3 p-2">Status Vise Count</Card.Title>
-        <Card.Body className="p-1">
-          <Row className="text-center">
-            <Col xs={2} md>
-              <div>
-                <small className="text-muted">200</small>
-                <h2 className="fw-bold text-success">{formatNumber(data.status_200 || 0)}</h2>
-              </div>
-            </Col>
-            <Col xs={2} md>
-              <div>
-                <small className="text-muted">502</small>
-                <h2 className="fw-bold text-danger">{formatNumber(data.status_502 || 0)}</h2>
-              </div>
-            </Col>
-            <Col xs={2} md>
-              <div>
-                <small className="text-muted">500</small>
-                <h2 className="fw-bold text-danger">{formatNumber(data.status_500 || 0)}</h2>
-              </div>
-            </Col>
-            <Col xs={2} md>
-              <div>
-                <small className="text-muted">400</small>
-                <h2 className="fw-bold text-info">{formatNumber(data.status_400 || 0)}</h2>
-              </div>
-            </Col>
-            <Col xs={2} md>
-              <div>
-                <small className="text-muted">404</small>
-                <h2 className="fw-bold text-success">{formatNumber(data.status_404 || 0)}</h2>
-              </div>
-            </Col>
-            <Col xs={2} md>
-              <div>
-                <small className="text-muted">401</small>
-                <h2 className="fw-bold text-info">{formatNumber(data.status_401 || 0)}</h2>
-              </div>
-            </Col>
-            <Col xs={2} md>
-              <div>
-                <small className="text-muted">403</small>
-                <h2 className="fw-bold text-info">{formatNumber(data.status_403 || 0)}</h2>
-              </div>
-            </Col>
-          </Row>
-        </Card.Body>
-      </Card>
+    <Card className="shadow-sm border-1 rounded-3 p-2 h-100">
+  <Card.Title className="mb-3 ps-2 fw-semibold">Status Wise Count</Card.Title>
+  <Card.Body className="p-1">
+    <Row className="text-center g-2">
+      {countStatusCodes.map((status, index) => (
+        <Col
+          key={index}
+          xs={4}   // 3 per row on mobile
+          sm={3}   // 4 per row on small tablets
+          md={2}   // 6 per row on medium screens
+          lg       // auto on large screens
+          className="d-flex flex-column align-items-center justify-content-center"
+        >
+          <small className="text-muted">{status.code}</small>
+          <h4 className={`fw-bold ${status.color} mb-0`}>
+            {formatNumber(status.value || 0)}
+          </h4>
+        </Col>
+      ))}
+    </Row>
+  </Card.Body>
+</Card>
     </React.Fragment>
   );
 };
